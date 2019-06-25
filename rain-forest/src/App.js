a2a12a9062abad9a143ff78eed92edc97c26ce05
surichen35 <siyuchen3538@163.com>
2019-06-25 14:11:03 +0800

    create antd project

---
+++ rain-forest/src/App.js
@@ -0,0 +1,20 @@
+import React , {Component}from 'react';
+import { Table, Popconfirm, Button } from 'antd';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+	render(){
+		 return (
+			<div className="App">
+			  <header className="App-header">
+				<h1>Welcome to RF!</h1>
+			  </header>
+			  <div className="App-header">
+				
+			  </div>
+			</div>
+		);
+	}
+}
+export default App;
